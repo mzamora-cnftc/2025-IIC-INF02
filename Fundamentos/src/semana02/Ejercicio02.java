154f88946a7ed6740e24629f0fb721a6af5e9118
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2025-05-28 00:55:23 +0000

    S02

---
+++ Fundamentos/src/semana02/Ejercicio02.java
@@ -0,0 +1,30 @@
+package semana02;
+
+import utilitarios.Utils;
+
+public class Ejercicio02 {
+
+    public static double PRECIO_UNITARIO = 10;
+    public static double COSTO_UNITARIO = 5.5;
+    public static void main(String[] args) {
+        int cantidadProducto = 0;
+        double utilidadUnitaria = 0.0, utilidadTotal = 0.0, utilidadHugo = 0.0, utilidadPaco = 0.0, utilidadLuis = 0.0;
+
+        Utils.limpiarConsola();
+
+        //inputs
+        cantidadProducto = Utils.leerRangosEnteros("Digite la cantidad de limonadas: ", 1, 100);
+
+        //process
+        utilidadUnitaria = PRECIO_UNITARIO - COSTO_UNITARIO;
+        utilidadTotal = utilidadUnitaria * cantidadProducto;
+        utilidadHugo = utilidadTotal * 0.4;
+        utilidadLuis = utilidadTotal * 0.3;
+        utilidadPaco = utilidadTotal * 0.3;
+
+        //out
+        System.out.println("Hugo: " + utilidadHugo);
+        System.out.println("Luis: " + utilidadLuis);
+        System.out.println("Paco: " + utilidadPaco);
+    }
+}
