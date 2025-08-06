8c7a54a74eb982fa7f2364092deb9b6f2d5dac21
mauricio-zamora <141055068+mzamora-cnftc@users.noreply.github.com>
2025-08-06 02:34:41 +0000

    S11

--- Fundamentos/src/semana13/Universidad.java
+++ Fundamentos/src/semana13/Universidad.java
@@ -4,6 +4,30 @@ import utilitarios.Utils;
 
 public class Universidad {
 
+    public static void cargarNumerosAleatorios(double[][] valores) {
+        for (int i = 0; i < valores.length; i++) {
+            for (int j = 0; j < valores[i].length; j++) {
+                valores[i][j] = Utils.generarNumerosEntre(60, 100);
+            }
+        }
+    }
+
+    public static void imprimir(String[] etiFilas, String[] etiCols, double[][] valores) {
+        System.out.printf("%-15s","ESTUDIANTE");
+        for (int i = 0; i < etiCols.length; i++) {
+            System.out.printf("%15s",etiCols[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < etiFilas.length; i++) {
+            System.out.printf("%-15s",etiFilas[i]);
+            for (int j = 0; j < etiCols.length; j++) {
+                System.out.printf("%15.2f",valores[i][j]);
+            }
+            System.out.println();
+            
+        }
+    }
+
 
     public static void llenarArregloString(String[] datos, String pregunta){
         System.out.println(pregunta);
@@ -18,19 +42,24 @@ public class Universidad {
         String[] pruebas = null;
         int nEstudiantes;
         int nPruebas;
-        
-        nEstudiantes = Utils.leerEntero("Digite la cantidad de estudiantes: ");
-        nPruebas = Utils.leerEntero("Digite la cantidad de pruebas: ");
 
-        estudiantes = new String[nEstudiantes];
-        pruebas = new String[nPruebas];
-
-        notas = new double[nEstudiantes][nPruebas];
+        Utils.limpiarConsola();
         
-        llenarArregloString(estudiantes, "Digite el nombre de los estudiantes");
-        llenarArregloString(pruebas, "Digite el nombre de las pruebas");
+        //nEstudiantes = Utils.leerEntero("Digite la cantidad de estudiantes: ");
+        //nPruebas = Utils.leerEntero("Digite la cantidad de pruebas: ");
 
+        //estudiantes = new String[nEstudiantes];
+        estudiantes = new String[] {"Paula", "Kath", "Trayce", "Linda"};
+        //pruebas = new String[nPruebas];
+        pruebas = new String[] {"Español", "Ciencias"};
         
+        notas = new double[estudiantes.length][pruebas.length];
+        cargarNumerosAleatorios(notas);
+        
+        //llenarArregloString(estudiantes, "Digite el nombre de los estudiantes");
+        //llenarArregloString(pruebas, "Digite el nombre de las pruebas");
+
+        imprimir(estudiantes, pruebas, notas);
 
         
     }
